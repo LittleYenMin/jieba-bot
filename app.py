e55596dd48f815e134be0b092122532679cd47ac
dannypsnl <dannypsnl@gmail.com>
2019-04-02 15:39:16 +0800

    fix pep8 E302 issue

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -36,12 +37,14 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = jieba.cut(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=", ".join(text)))
+
 
 if __name__ == "__main__":
     app.run()
