845f84127350b9654953aba25ed2ac50e013d345
LittleYenMin <fkym08@gmail.com>
2019-04-02 15:09:24 +0800

    setup: heroku variable

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN', ''))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', ''))
+line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
 @app.route("/callback", methods=['POST'])
 def callback():
