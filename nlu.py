357c2d262580690e44b91ea258e06bb0f78fd849
dannypsnl <dannypsnl@gmail.com>
2019-04-05 01:54:35 +0800

    get command by input text and run to get response
      not implement run command yet

--- nlu.py
+++ nlu.py
@@ -1,5 +1,24 @@
 import jieba
 
+
 def response_message(text: str) -> str:
-    split_text = jieba.cut(text)
-    return ", ".join(split_text)
+    cmd = _get_command(jieba.cut(text))
+    return _exec_command(cmd)
+
+
+def _get_command(text_sequence: [str]) -> str:
+    for text in text_sequence:
+        if text == '曠課':
+            return '昌昌;曠課'
+
+
+def _exec_command(cmd: str) -> str:
+    """exec the command and return response
+
+    Parameters:
+        cmd (str): The string represented command
+
+    Returns:
+        response (str): The string for text response
+    """
+    pass
