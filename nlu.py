1598b9c1984deeb310506cbd1a9460fba6a2dba6
= <=>
2019-04-05 14:24:56 +0800

    update: multiple intents

--- nlu.py
+++ nlu.py
@@ -1,18 +1,22 @@
 import jieba
 
+command_mapping = {'曠課': '昌昌;曠課', '課表': '昌昌;課表', '成績': '昌昌;成績'}
+
 
 def response_message(text: str) -> str:
-    cmd = _get_command(jieba.cut(text))
-    return _exec_command(cmd)
+    cmds = _get_commands(jieba.cut(text))
+    return _exec_command(cmds)
 
 
-def _get_command(text_sequence: [str]) -> str:
+def _get_commands(text_sequence: [str]) -> set:
+    commands = set()
     for text in text_sequence:
-        if text == '曠課':
-            return '昌昌;曠課'
+        if text in command_mapping:
+            commands.add(command_mapping.get(text))
+    return commands
 
 
-def _exec_command(cmd: str) -> str:
+def _exec_command(cmds: set) -> str:
     """exec the command and return response
 
     Parameters:
