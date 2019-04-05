05459b9b7d268b98320a8f4f50d26ad82faaf925
邱彥銘 <fkym08@gmail.com>
2019-04-05 14:47:12 +0800

    get command by input text and run to get response (#7)
    * get command by input text and run to get response
    
      not implement run command yet
    
    * update: multiple intents
    
    * ignore: idea
    
    * update: make it readable and flexible

--- nlu.py
+++ nlu.py
@@ -1,5 +1,32 @@
 import jieba
 
-def response_message(text: str) -> str:
-    split_text = jieba.cut(text)
-    return ", ".join(split_text)
+command_map = {'曠課': '昌昌;曠課', '課表': '昌昌;課表', '成績': '昌昌;成績'}
+
+
+def response_message(text: str) -> list:
+    messages = []
+    cmds = _get_commands(jieba.cut(text))
+    for cmd in cmds:
+        messages.append(_exec_command(cmd))
+    return messages
+
+
+def _get_commands(text_sequence: [str]) -> set:
+    possible_commands = set()
+    for text in text_sequence:
+        cmd = command_map.get(text)
+        if cmd is not None:
+            possible_commands.add(cmd)
+    return possible_commands
+
+
+def _exec_command(cmd: str) -> str:
+    """exec the command and return response
+
+    Parameters:
+        cmd (str): The string represented command
+
+    Returns:
+        response (str): The string for text response
+    """
+    pass
