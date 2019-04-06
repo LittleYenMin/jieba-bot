2be11650aefcf2ff7f0cbd0af5c2836377bec3dd
LittleYenMin <fkym08@gmail.com>
2019-04-06 14:02:54 +0800

    update: clear test case for cosine similarity.

--- tests/test_cosine_similarity.py
+++ tests/test_cosine_similarity.py
@@ -3,10 +3,11 @@ import nlu
 
 
 def test_cosine_similarity():
-    question = ['a', 'b']
-    user_input = ['a', 'c']
-    other_input = ['c']
-    user_input_similarity = nlu._consine_similarity(question, user_input)
-    other_input_similarity = nlu._consine_similarity(question, other_input)
+    question_segment = ['我', '要', '查', '成績']
+    user_input_segment = ['我', '想', '查', '成績']
+    other_input_segment = ['我', '成績', '沒', '考', '好']
+    user_input_similarity = nlu._consine_similarity(question_segment, user_input_segment)
+    other_input_similarity = nlu._consine_similarity(question_segment, other_input_segment)
     # user input is more similar then other_input
+    print(user_input_similarity, other_input_similarity)
     assert user_input_similarity > other_input_similarity
