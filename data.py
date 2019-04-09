ff31987eccbdf48798f1c6df22b006454fc5002a
LittleYenMin <fkym08@gmail.com>
2019-04-09 11:33:05 +0800

    add: data source to read from csv file.

---
+++ data.py
@@ -0,0 +1,15 @@
+import csv
+from questions import Questions
+
+class Source:
+
+    @staticmethod
+    def from_csv(path: str = './data.csv') -> [Questions]:
+        questions = []
+        with open(path, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            # headers
+            _ = next(reader, None)
+            for row in reader:
+                questions.append(Questions(question=row[0], answer=row[1]))
+        return questions
