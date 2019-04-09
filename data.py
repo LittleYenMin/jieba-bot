c343d2a2386f94d824009c50d2a0efac3c04eec5
LittleYenMin <fkym08@gmail.com>
2019-04-09 16:37:32 +0800

    rename: question to ExampleQuestion.

--- data.py
+++ data.py
@@ -1,19 +1,19 @@
 from csv import reader as csv_reader
 
-from questions import Questions
+from questions import ExampleQuestion
 
 
 class Source:
 
     @staticmethod
-    def from_csv(path: str) -> [Questions]:
+    def from_csv(path: str) -> [ExampleQuestion]:
         questions = []
         with open(path, 'r', encoding='utf-8') as f:
             csv = csv_reader(f)
             next(csv, None)  # escape headers is useless now
             for row in csv:
                 questions.append(
-                    Questions(
+                    ExampleQuestion(
                         question=row[0],
                         command_type=row[1]))
         return questions
