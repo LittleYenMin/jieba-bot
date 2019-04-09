2372f31e2e6621342e1c799caed9def9676c6d17
LittleYenMin <fkym08@gmail.com>
2019-04-09 15:45:16 +0800

    remove: header variable to keep code clean and simple.

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ class Source:
         questions = []
         with open(path, 'r', encoding='utf-8') as f:
             csv = csv_reader(f)
-            _ = next(csv, None)  # escape headers is useless now
+            next(csv, None)  # escape headers is useless now
             for row in csv:
                 questions.append(Questions(question=row[0], answer=row[1]))
         return questions
