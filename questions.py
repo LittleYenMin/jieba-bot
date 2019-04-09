4f19d9c6e341fcafd40536c1df16d93032b951da
LittleYenMin <fkym08@gmail.com>
2019-04-09 11:32:24 +0800

    add: question object(schema)

---
+++ questions.py
@@ -0,0 +1,8 @@
+class Questions:
+
+    def __init__(self, question: str, answer: str):
+        self.question = question
+        self.answer = answer
+
+    def __repr__(self):
+        return '<Questions {question} {answer}>'.format(question=self.question, answer=self.answer)
