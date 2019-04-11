f6a59e42c43a8aca407089a6ccb13cb627d38456
LittleYenMin <fkym08@gmail.com>
2019-04-11 16:22:19 +0800

    remove: from import syntax.

--- data/source.py
+++ data/source.py
@@ -1,20 +1,20 @@
 import csv
 
-from questions import ExampleQuestion
+import questions
 
 
-def get_from_csv_file(path: str) -> [ExampleQuestion]:
+def from_csv_file(path: str) -> [questions.ExampleQuestion]:
     with open(path, 'r', encoding='utf-8') as f:
         return from_csv(f)
 
 
-def from_csv(iterable: any) -> [ExampleQuestion]:
-    questions = []
+def from_csv(iterable: any) -> [questions.ExampleQuestion]:
+    result = []
     csv_reader = csv.reader(iterable)
     next(csv_reader, None)  # escape headers is useless now
     for row in csv_reader:
-        questions.append(
-            ExampleQuestion(
+        result.append(
+            questions.ExampleQuestion(
                 question=row[0],
                 command_type=row[1]))
-    return questions
+    return result
