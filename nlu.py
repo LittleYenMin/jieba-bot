e23419b02978437572a8f2f4d9043a259f2cedcc
LittleYenMin <fkym08@gmail.com>
2019-04-11 15:01:28 +0800

    add: method similarity in nlu (split text with jieba and compare).

--- nlu.py
+++ nlu.py
@@ -4,21 +4,8 @@ import jieba
 command_map = {'曠課': '昌昌;曠課', '課表': '昌昌;課表', '成績': '昌昌;成績'}
 
 
-def response_message(text: str) -> list:
-    messages = []
-    cmds = _get_commands(jieba.cut(text))
-    for cmd in cmds:
-        messages.append(_exec_command(cmd))
-    return messages
-
-
-def _get_commands(text_sequence: [str]) -> set:
-    possible_commands = set()
-    for text in text_sequence:
-        cmd = command_map.get(text)
-        if cmd is not None:
-            possible_commands.add(cmd)
-    return possible_commands
+def similarity(a: str, b: str) -> float:
+    return _similarity(jieba.lcut(a), jieba.lcut(b))
 
 
 def _similarity(a: [str], b: [str]) -> float:
